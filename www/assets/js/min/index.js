93f316af5f0898dba3e2f503ed38d25c0b767c0d
VICTOR VALENCIA <victor.valencia@gmail.com>
2016-03-06 18:20:56 -0500

    Reorder FB

--- www/assets/js/min/index.js
+++ www/assets/js/min/index.js
@@ -33,12 +33,12 @@ var app = {
     // The scope of 'this' is the event. In order to call the 'receivedEvent'
     // function, we must explicitly call 'app.receivedEvent(...);'
     onDeviceReady: function() {
-        alert("Device");
-        facebookConnectPlugin.browserInit('1691694321105222');
+        alert("Device");        
         app.receivedEvent('deviceready');
     },
     // Update DOM on a Received Event
     receivedEvent: function(id) {
+        facebookConnectPlugin.browserInit('1691694321105222');
         getDeviceProperty();
     }
 };
