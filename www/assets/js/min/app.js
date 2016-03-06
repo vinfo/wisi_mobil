ecccadd5c960e8ac140706f338b3aa937d0ecb06
VICTOR VALENCIA <victor.valencia@gmail.com>
2016-03-06 17:46:42 -0500

    FB

--- www/assets/js/min/app.js
+++ www/assets/js/min/app.js
@@ -44,7 +44,7 @@ var fbLoginSuccess = function (userData) {
 $$("body").on("click", ".button-facebook", function() {
     myApp.alert("Conectar FB", "");
     $.ajaxSetup({ cache: true });
-    $.getScript('//connect.facebook.net/en_US/sdk.js', function(){
+    $.getScript('https://connect.facebook.net/en_US/sdk.js', function(){
         FB.init({
             appId: '1691694321105222',
           version: 'v2.5' // or v2.0, v2.1, v2.2, v2.3
