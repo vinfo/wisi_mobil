87b72b86db9d097d25ab2720c0c5d1a838a4dc53
VICTOR VALENCIA <victor.valencia@gmail.com>
2016-03-06 19:03:11 -0500

    Ready

--- www/js/index.js
+++ www/js/index.js
@@ -37,18 +37,15 @@ var app = {
     },
     // Update DOM on a Received Event
     receivedEvent: function(id) {
-        alert(2);
-        if (window.cordova.platformId == "browser") {
-            facebookConnectPlugin.browserInit('1691694321105222');
-            alert("Registra");
-        }        
+        alert(2);        
+        facebookConnectPlugin.browserInit('1691694321105222');
+        alert("Registra");
+               
         var parentElement = document.getElementById(id);
         var listeningElement = parentElement.querySelector('.listening');
         var receivedElement = parentElement.querySelector('.received');
 
         listeningElement.setAttribute('style', 'display:none;');
         receivedElement.setAttribute('style', 'display:block;');
-
-        console.log('Received Event: ' + id);
     }
 };
