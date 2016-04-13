40ea6aa3baf4c90a4ba86edb16782bb3612f1218
VICTOR VALENCIA <victor.valencia@gmail.com>
2016-04-13 10:35:51 -0500

    param app

--- www/assets/js/min/app.js
+++ www/assets/js/min/app.js
@@ -268,18 +268,15 @@ $$("body").on("click", ".button-facebook", function() {
 });
 
 $$("body").on("click", ".pautar", function() {
-    localStorage.setItem("app","mobil");
     window.open("http://wisi.com.co/public/#/", "_system");
 });
 $$("body").on("click", ".free-navegate", function() {
     setUserRadius();
-    localStorage.setItem("app","mobil");
-    window.open("http://wisi.com.co/public/#/ad/1/"+localStorage.userid+"?navegate=free", "_system");
+    window.open("http://wisi.com.co/public/#/ad/1/"+localStorage.userid+"?navegate=free&app=mobil", "_system");
 });
 $$("body").on("click", ".pay-navegate", function() {
     setUserRadius();
-    localStorage.setItem("app","mobil");
-    window.open("http://wisi.com.co/public/#/ad/1/"+localStorage.userid+"?navegate=pay", "_system");
+    window.open("http://wisi.com.co/public/#/ad/1/"+localStorage.userid+"?navegate=pay&app=mobil", "_system");
 });
 
 $$("body").on("click", ".close_sesion", function() {
