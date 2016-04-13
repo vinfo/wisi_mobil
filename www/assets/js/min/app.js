5e9d54b68426e57ed9d814722072986cdfd5cb77
VICTOR VALENCIA <victor.valencia@gmail.com>
2016-04-13 13:13:33 -0500

    list

--- www/assets/js/min/app.js
+++ www/assets/js/min/app.js
@@ -185,7 +185,7 @@ function getRewardsData(id) {
             if(obj.status){
               for(var k in obj.data) {
                   var o = obj.data[k];      
-                  $("#rewards").prepend('<li class="list-re mt-0 mb-0 nice-list"><div class="item-inner"><div class="title-re">Tipo: '+o.type+'</div><div class="nice-list">Fecha/Hora: '+o.datereg+'<br/>Vence: '+o.expired+'<br/>Tiempo: '+o.time+' mins.<br/>Gastado: '+o.time_spend+' mins.<br/>Estado: '+o.status+'.<br/></div></div></li>');
+                  $("#rewards").prepend('<li class="list-re mt-0 mb-0 nice-list"><div class="item-inner"><div class="nice-list">Tipo: '+o.type+'<br/>Fecha/Hora: '+o.datereg+'<br/>Vence: '+o.expired+'<br/>Tiempo: '+o.time+' mins.<br/>Gastado: '+o.time_spend+' mins.<br/>Estado: '+o.status+'.<br/></div></div></li>');
               }
             }
         }
