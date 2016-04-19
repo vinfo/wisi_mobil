2d06192b106e51cbfef1d53bbe45c2bcdb22a5ca
VICTOR VALENCIA <victor.valencia@gmail.com>
2016-04-19 15:27:59 -0500

    Sound

--- www/assets/js/min/index.js
+++ www/assets/js/min/index.js
@@ -114,7 +114,7 @@ function getDeviceProperty()
     for(var i=0; i<a.length; i++){
       if((a[i].search("WISI TE CONECTA")>0||a[i].search("VALENCIA_V")>0)&&exists==0){
         if(localStorage.wisi=="false"){
-            cordova.plugins.notification.local.schedule({ message:"Red WISI detectada" });
+            cordova.plugins.notification.local.schedule({ message:"Red WISI detectada",sound: "file://sounds/HTC Happy.mp3" });
             //myApp.alert("Red WISI detectada", "");
             localStorage.setItem("wisi","true"); 
             //navigator.vibrate(1000);
